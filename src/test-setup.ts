f04ce01db9f592d3741be6ed0cc1009fac7cf064
Danilo Andrade <danilotandrade1518@gmail.com>
2025-12-16 16:58:58 -0300

    refactor: Atualizar módulo de detecção de mudanças para versão sem zona
    - Substituído o módulo de detecção de mudanças com zona por um módulo de detecção de mudanças sem zona, melhorando a performance dos testes.
    - Ajustadas as importações e a inicialização do ambiente de testes para refletir a nova abordagem, garantindo uma configuração mais eficiente.
    
    Essas alterações visam otimizar a estrutura dos testes e a performance da aplicação.

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -1,13 +1,15 @@
-import { provideZoneChangeDetection, NgModule } from "@angular/core";
+import { provideZonelessChangeDetection, NgModule } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing';
 
-@NgModule({ providers: [ provideZoneChangeDetection() ] })
-export class ZoneChangeDetectionModule {}
-
+@NgModule({ providers: [provideZonelessChangeDetection()] })
+export class ZonelessChangeDetectionModule {}
 
 try {
-  TestBed.initTestEnvironment([ZoneChangeDetectionModule, BrowserTestingModule], platformBrowserTesting());
+  TestBed.initTestEnvironment(
+    [ZonelessChangeDetectionModule, BrowserTestingModule],
+    platformBrowserTesting()
+  );
 } catch {
   console.log('Ambiente de testes já inicializado');
 }
