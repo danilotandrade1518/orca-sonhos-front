f3ae0128125eb16e0a2de17732c83b6e327f0d92
Danilo Andrade <danilotandrade1518@gmail.com>
2025-12-16 16:15:28 -0300

    feat: Atualizar dependências do Angular e bibliotecas relacionadas
    - Atualizadas as versões do Angular para 21.0.5, incluindo `@angular/common`, `@angular/compiler`, `@angular/core`, `@angular/forms`, `@angular/platform-browser` e `@angular/router`.
    - Atualizadas as dependências de desenvolvimento do Angular para 21.0.3 e 21.0.5, incluindo `@angular-devkit/build-angular`, `@angular/build`, `@angular/cli` e `@angular/compiler-cli`.
    - Atualizadas várias bibliotecas, como `@algolia` e `vitest`, para suas versões mais recentes, melhorando a compatibilidade e a segurança.
    
    Essas alterações visam garantir que o projeto utilize as versões mais recentes e estáveis das bibliotecas, melhorando a performance e a segurança da aplicação.

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -1,8 +1,13 @@
+import { provideZoneChangeDetection, NgModule } from "@angular/core";
 import { TestBed } from '@angular/core/testing';
 import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing';
 
+@NgModule({ providers: [ provideZoneChangeDetection() ] })
+export class ZoneChangeDetectionModule {}
+
+
 try {
-  TestBed.initTestEnvironment(BrowserTestingModule, platformBrowserTesting());
+  TestBed.initTestEnvironment([ZoneChangeDetectionModule, BrowserTestingModule], platformBrowserTesting());
 } catch {
   console.log('Ambiente de testes já inicializado');
 }
